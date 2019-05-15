8ce76f3d34482e5dbc4f52b038dd7cd02aefbe99
OMIsie <omisie@gmail.com>
2019-05-15 13:06:55 +0200

    Dodano parametry i gettery Eventu

--- src/main/java/pw/elka/simulator/TKEvent.java
+++ src/main/java/pw/elka/simulator/TKEvent.java
@@ -5,19 +5,32 @@ package pw.elka.simulator;
  */
 public class TKEvent {
 
-    private final Number time;
+    private final Number timeOfArrival;
+    private final Number timeOfResidence;
+    // ToDo: Dodać możliwe wartości statusu i ewentualnie zmienić jego typ
+    private final String eventStatus;
     private final TKEventType type;
 
-    public TKEvent(Number time, TKEventType type) {
-        this.time = time;
+    public TKEvent(Number timeOfArrival, Number timeOfResidence, String eventStatus, TKEventType type) {
+        this.timeOfArrival = timeOfArrival;
+        this.timeOfResidence = timeOfResidence;
+        this.eventStatus = eventStatus;
         this.type = type;
     }
 
-    public Number getTime() {
-        return time;
+    public Number getTimeOfArrival() {
+        return timeOfArrival;
     }
+
+    public Number getTimeOfResidence() {
+        return timeOfResidence;
+    }
+
+    public String getEventStatus() { return eventStatus; }
 
     public TKEventType getType() {
         return type;
     }
+
+    // ToDo: settery jak u Olka
 }
--- src/main/java/pw/elka/simulator/TKEventType.java
+++ src/main/java/pw/elka/simulator/TKEventType.java
@@ -5,6 +5,8 @@ package pw.elka.simulator;
  * TKEventType Narazie tu nic nie ma
  */
 public enum TKEventType {
-    A, B, C, D, E, F, G, H // Niewiem ile tego ma byćs
+    // A, B, C, D, E, F, G, H // Niewiem ile tego ma byćs
+
+    // Nie mam pojęcia jaką funkcję w ogóle te typy pełnią, CHYBA ZBĘDNE - Oskar
 
 }
